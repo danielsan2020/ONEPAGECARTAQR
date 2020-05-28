12df74dadcafd3f72584262c6f1c6048ea61d86c
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-05-28 14:15:14 -0600

    mailer

--- contact_process.php
+++ contact_process.php
@@ -1,19 +1,43 @@
 <?php
 
-    $to = "info@menuonlineqr.com";
+	require("class.phpmailer.php");
+	require("class.smtp.php");
+
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Port = 587; 
+	$mail->IsHTML(true); 
+	$mail->CharSet = "utf-8";
+
+	$to = "ricardo.rdzalt@gmail.com";	//Correo a donde llegaran los mails
+
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
+    $csubject = $_REQUEST['subject'];
     $cmessage = $_REQUEST['message'];
 
+	$smtpHost = "smtp.gmail.com";	//SMTP del servicio de correos
+	$smtpUsuario = "rikardo150@gmail.com";	//Correo desde donde será enviado
+	$smtpClave = "";	//Password del correo desde donde será enviado
+
+	$mail->Host = $smtpHost; 
+	$mail->Username = $smtpUsuario; 
+	$mail->Password = $smtpClave;
+	$mail->From = $from;
+	$mail->FromName = $name;
+	$mail->AddAddress($to); // Esta es la dirección a donde enviamos los datos del formulario
+
+	/*
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    $subject = "Tienes un mensaje de Menú OnlineQR";
+	*/
+	$subject = "Tienes un mensaje de Menú OnlineQR";
+	$mail->Subject = $subject;
+	
     $link = 'https://www.menuonlineqr.com/';
 
 	$body = "<!DOCTYPE html><html lang='es'><head><meta charset='UTF-8'><title>Menú OnlineQR</title></head><body>";
@@ -30,7 +54,9 @@
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
-	$send = mail($to, $subject, $body, $headers);
+	$mail->Body = $body;
+	
+	$mail->send();
 	
 	header('location:./contacto.html');
 
